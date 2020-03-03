51c77e52b51b4c9aa05ce26b969f21355f1ec749
test <xxx@google.com>
2020-03-03 16:54:22 +0700

    Done!!!

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 //Write the definition of myString() here
+void myString(char *&p,int n){
+	p=new char [n];
+	for(int i=0;i<n;i++){
+		p[i]='A'+i;
+	}
+	
+	
+	
+}
 
 int main(){
 	char *p;
--- lab16_3.cpp
+++ lab16_3.cpp
@@ -22,3 +22,25 @@ int main(){
 }
 
 //Write Definition of randomVector() showVector() and dotProduct() here
+vector<int> randomVector(int n){
+	vector<int> out(n);
+	for(int i=0;i<n;i++){
+		out[i]=rand()%10;
+	}
+	return out;
+}
+void showVector(vector<int> x){
+	cout << "[";
+	for(int i=0;i<x.size();i++){
+		cout << x[i] << " ";
+	}
+	cout << "]";
+}
+
+int dotProduct(vector<int> x,vector<int> y){
+	int sum=0;
+	for(int i=0;i<x.size();i++){
+		sum+=x[i]*y[i];
+	}
+	return sum;
+}
